3ba779f2ab6d3546e10f048c9bba03f85b2930eb
Gytura <gytura.zyl@gmail.com>
2024-01-23 00:35:02 +0100

    Add files via upload

---
+++ Regression Female.R
@@ -0,0 +1,38 @@
+load("nlsy97.rdata")
+dat = subset(
+  nlsy97,
+  wage > 0 &
+  ASVAB > 0 & 
+  hdegree > 0 &
+  weight2017 > 0 &
+  health2017 > 0 &
+  height2002 > 0 &
+  tenure2017 > 0 &
+  whours > 0 &
+  female == 1
+)
+
+
+dat$weight = dat$weight2017
+dat$height = dat$height2002 / 100
+dat$health = dat$health2017
+dat$BMI = dat$weight / (dat$height)^2
+dat$white_BMI = dat$white * dat$BMI
+dat$black_BMI = dat$black * dat$BMI
+dat$mixed_BMI = dat$mixed * dat$BMI
+dat$hispanic_BMI = dat$hispanic * dat$BMI
+dat$ln_wage = log(dat$wage)
+dat$tenure = dat$tenure2017
+dat$tenure_sq = (dat$tenure)^2
+
+
+if(T){
+  model_female = lm( 
+    ln_wage ~ 
+      1 + height + BMI + black + hispanic + mixed + black_BMI + hispanic_BMI + mixed_BMI + hdegree + tenure + ASVAB + tenure_sq,
+    data = dat
+    )
+}
+
+sink("")
+summary(model_female)
---
+++ Regression Male.R
@@ -0,0 +1,32 @@
+load("nlsy97.rdata")
+dat = subset(
+  nlsy97,
+  wage > 0 &
+    ASVAB > 0 & 
+    hdegree > 0 &
+    weight2017 > 0 &
+    health2017 > 0 &
+    height2002 > 0 &
+    tenure2017 >0 &
+    male == 1
+)
+
+
+dat$weight = dat$weight2017
+dat$height = dat$height2002 / 100
+dat$BMI = dat$weight / (dat$height)^2
+dat$white_BMI = dat$white * dat$BMI
+dat$black_BMI = dat$black *dat$BMI
+dat$ln_wage = log(dat$wage)
+dat$tenure = dat$tenure2017
+dat$tenure_sq = (dat$tenure)^2
+
+if(T){
+  model_male = lm( 
+    ln_wage ~ 
+      1 +  BMI + white_BMI + black_BMI + hdegree + tenure + tenure_sq,
+    data = dat
+  )
+}
+
+summary(model_male)
